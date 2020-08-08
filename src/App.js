d7614ac729e0666f19e6f698b8a38c7e40dbdece
badroezaman <mail.badroe@gmail.com>
2020-08-08 16:15:35 +0700

    new repo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+import UserInfo from "./UserInfo";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <UserInfo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <header>
+          <div className="container">
+            <h1 className="Title">Sanbercode Reactjs Week 3</h1>
+          </div>
+        </header>
+      </div>
+    </>
+  );
+};
+
+export default Header;
